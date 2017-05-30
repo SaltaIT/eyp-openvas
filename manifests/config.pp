a53b617bff0d02cf27deb6f4e11aa39752d365ba
Jordi Prats <jprats@systemadmin.es>
2017-05-30 14:19:45 +0200

    template

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,3 +1,11 @@
 class openvas::config inherits openvas {
 
+  file { '/etc/openvas/openvassd.conf':
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0640',
+    content => template("${module_name}/openvassd.erb"),
+  }
+
 }
