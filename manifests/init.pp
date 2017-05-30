ed7a585d0669a1be5ab672b04d52ec0cdb77e723
Jordi Prats <jprats@systemadmin.es>
2017-05-30 14:20:37 +0200

    plugins folder

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,6 +5,7 @@ class openvas(
                             $manage_docker_service = true,
                             $service_ensure        = 'running',
                             $service_enable        = true,
+                            $plugins_folder        = '/var/lib/openvas/plugins',
                           ) inherits openvas::params{
 
   validate_re($package_ensure, [ '^present$', '^installed$', '^absent$', '^purged$', '^held$', '^latest$' ], 'Not a supported package_ensure: present/absent/purged/held/latest')
